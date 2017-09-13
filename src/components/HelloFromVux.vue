e5cfb5cecb58c9e51fdc30ac511e5396f3ff805b
zhujy <zhujy@lianchuang.com>
2017-09-13 17:39:42 +0800

    添加框架，vue+vux+webpack

---
+++ src/components/HelloFromVux.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="vux-demo">
+      <img class="logo" src="../assets/vux_logo.png">
+      <h1> </h1>
+    </div>
+    <group title="cell demo">
+      <cell title="VUX" value="cool" is-link></cell>
+    </group>
+    <group>
+      <calendar title="title" v-model="value"></calendar>
+    </group>
+  </div>
+</template>
+
+<script>
+import { Group, Cell, Calendar } from 'vux'
+
+export default {
+  components: {
+    Group,
+    Cell,
+    Calendar
+  },
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style>
+.vux-demo {
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
